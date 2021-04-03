fd6539be32afed0b9f402a29978332dc827d5e50
Brad Ringel <brad.ringel@gmail.com>
2021-04-02 22:20:21 -0400

    scrape watch provider data from movie db to be able to store in settings

---
+++ src/data/watchProviders.ts
@@ -0,0 +1,738 @@
+/**
+ * generated from this script running on https://www.themoviedb.org/settings/streaming
+ * providers = document.querySelectorAll('#ott_providers li:not(.hidden)')
+ * const watchProviders = Array.from(providers).map(provider => {
+ *   const logo = provider.querySelector('img');
+ *   return {
+ *     id: Number.parseInt(provider.dataset['value'], 10),
+ *     name: logo.alt
+ *   }
+ * })
+ */
+
+export const watchProviders = [
+  {
+    id: 8,
+    name: 'Netflix'
+  },
+  {
+    id: 119,
+    name: 'Amazon Prime Video'
+  },
+  {
+    id: 9,
+    name: 'Amazon Prime Video'
+  },
+  {
+    id: 337,
+    name: 'Disney Plus'
+  },
+  {
+    id: 2,
+    name: 'Apple iTunes'
+  },
+  {
+    id: 3,
+    name: 'Google Play Movies'
+  },
+  {
+    id: 257,
+    name: 'fuboTV'
+  },
+  {
+    id: 11,
+    name: 'Mubi'
+  },
+  {
+    id: 15,
+    name: 'Hulu'
+  },
+  {
+    id: 100,
+    name: 'GuideDoc'
+  },
+  {
+    id: 384,
+    name: 'HBO Max'
+  },
+  {
+    id: 175,
+    name: 'Netflix Kids'
+  },
+  {
+    id: 386,
+    name: 'Peacock'
+  },
+  {
+    id: 283,
+    name: 'Crunchyroll'
+  },
+  {
+    id: 387,
+    name: 'Peacock Premium'
+  },
+  {
+    id: 87,
+    name: 'Acorn TV'
+  },
+  {
+    id: 10,
+    name: 'Amazon Video'
+  },
+  {
+    id: 350,
+    name: 'Apple TV Plus'
+  },
+  {
+    id: 192,
+    name: 'YouTube'
+  },
+  {
+    id: 309,
+    name: 'Sun Nxt'
+  },
+  {
+    id: 464,
+    name: 'Kocowa'
+  },
+  {
+    id: 188,
+    name: 'YouTube Premium'
+  },
+  {
+    id: 190,
+    name: 'Curiosity Stream'
+  },
+  {
+    id: 235,
+    name: 'YouTube Free'
+  },
+  {
+    id: 521,
+    name: 'Spamflix'
+  },
+  {
+    id: 27,
+    name: 'HBO Now'
+  },
+  {
+    id: 495,
+    name: 'CGood TV'
+  },
+  {
+    id: 531,
+    name: 'Paramount Plus'
+  },
+  {
+    id: 269,
+    name: 'Funimation Now'
+  },
+  {
+    id: 475,
+    name: 'DOCSVILLE'
+  },
+  {
+    id: 105,
+    name: 'FandangoNOW'
+  },
+  {
+    id: 534,
+    name: 'Argo'
+  },
+  {
+    id: 238,
+    name: 'IMDb TV'
+  },
+  {
+    id: 546,
+    name: 'WOW Presents Plus'
+  },
+  {
+    id: 207,
+    name: 'The Roku Channel'
+  },
+  {
+    id: 551,
+    name: 'Magellan TV'
+  },
+  {
+    id: 212,
+    name: 'Hoopla'
+  },
+  {
+    id: 554,
+    name: 'BroadwayHD'
+  },
+  {
+    id: 83,
+    name: 'The CW'
+  },
+  {
+    id: 559,
+    name: 'Filmzie'
+  },
+  {
+    id: 206,
+    name: 'CW Seed'
+  },
+  {
+    id: 7,
+    name: 'Vudu'
+  },
+  {
+    id: 332,
+    name: 'VUDU Free'
+  },
+  {
+    id: 43,
+    name: 'Starz'
+  },
+  {
+    id: 258,
+    name: 'Criterion Channel'
+  },
+  {
+    id: 37,
+    name: 'Showtime'
+  },
+  {
+    id: 209,
+    name: 'PBS'
+  },
+  {
+    id: 177,
+    name: 'Pantaflix'
+  },
+  {
+    id: 123,
+    name: 'FXNow'
+  },
+  {
+    id: 73,
+    name: 'Tubi TV'
+  },
+  {
+    id: 191,
+    name: 'Kanopy'
+  },
+  {
+    id: 243,
+    name: 'Comedy Central'
+  },
+  {
+    id: 68,
+    name: 'Microsoft Store'
+  },
+  {
+    id: 78,
+    name: 'CBS'
+  },
+  {
+    id: 279,
+    name: 'Redbox'
+  },
+  {
+    id: 139,
+    name: 'Max Go'
+  },
+  {
+    id: 148,
+    name: 'ABC'
+  },
+  {
+    id: 358,
+    name: 'DIRECTV'
+  },
+  {
+    id: 12,
+    name: 'Crackle'
+  },
+  {
+    id: 80,
+    name: 'AMC'
+  },
+  {
+    id: 25,
+    name: 'Fandor'
+  },
+  {
+    id: 122,
+    name: 'Hotstar'
+  },
+  {
+    id: 79,
+    name: 'NBC'
+  },
+  {
+    id: 34,
+    name: 'Epix'
+  },
+  {
+    id: 211,
+    name: 'Freeform'
+  },
+  {
+    id: 155,
+    name: 'History'
+  },
+  {
+    id: 215,
+    name: 'Syfy'
+  },
+  {
+    id: 156,
+    name: 'A&E'
+  },
+  {
+    id: 157,
+    name: 'Lifetime'
+  },
+  {
+    id: 99,
+    name: 'Shudder'
+  },
+  {
+    id: 185,
+    name: 'Screambox'
+  },
+  {
+    id: 143,
+    name: 'Sundance Now'
+  },
+  {
+    id: 241,
+    name: 'Popcornflix'
+  },
+  {
+    id: 151,
+    name: 'BritBox'
+  },
+  {
+    id: 14,
+    name: 'realeyz'
+  },
+  {
+    id: 247,
+    name: 'Pantaya'
+  },
+  {
+    id: 248,
+    name: 'Boomerang'
+  },
+  {
+    id: 251,
+    name: 'Urban Movie Channel'
+  },
+  {
+    id: 268,
+    name: 'History Vault'
+  },
+  {
+    id: 254,
+    name: 'Dove Channel'
+  },
+  {
+    id: 255,
+    name: 'Yupp TV'
+  },
+  {
+    id: 218,
+    name: 'Eros Now'
+  },
+  {
+    id: 259,
+    name: 'Magnolia Selects'
+  },
+  {
+    id: 260,
+    name: 'WWE Network'
+  },
+  {
+    id: 264,
+    name: 'MyOutdoorTV'
+  },
+  {
+    id: 261,
+    name: 'Nickhits Amazon Channel'
+  },
+  {
+    id: 262,
+    name: 'Noggin Amazon Channel'
+  },
+  {
+    id: 267,
+    name: 'Hopster TV'
+  },
+  {
+    id: 275,
+    name: 'Laugh Out Loud'
+  },
+  {
+    id: 276,
+    name: 'Smithsonian Channel'
+  },
+  {
+    id: 278,
+    name: 'Pure Flix'
+  },
+  {
+    id: 281,
+    name: 'Hallmark Movies'
+  },
+  {
+    id: 284,
+    name: 'Lifetime Movie Club'
+  },
+  {
+    id: 293,
+    name: 'PBS Kids Amazon Channel'
+  },
+  {
+    id: 288,
+    name: 'Boomerang Amazon Channel'
+  },
+  {
+    id: 289,
+    name: 'Cinemax Amazon Channel'
+  },
+  {
+    id: 292,
+    name: 'Pantaya Amazon Channel'
+  },
+  {
+    id: 290,
+    name: 'Hallmark Movies Now Amazon Channel'
+  },
+  {
+    id: 294,
+    name: 'PBS Masterpiece Amazon Channel'
+  },
+  {
+    id: 295,
+    name: 'Viewster Amazon Channel'
+  },
+  {
+    id: 291,
+    name: 'MZ Choice Amazon Channel'
+  },
+  {
+    id: 299,
+    name: 'Sling TV'
+  },
+  {
+    id: 300,
+    name: 'Pluto TV'
+  },
+  {
+    id: 200,
+    name: 'HBO Now Amazon Channel'
+  },
+  {
+    id: 203,
+    name: 'Showtime Amazon Channel'
+  },
+  {
+    id: 198,
+    name: 'CBS All Access Amazon Channel'
+  },
+  {
+    id: 430,
+    name: 'HiDive'
+  },
+  {
+    id: 455,
+    name: 'Night Flight Plus'
+  },
+  {
+    id: 454,
+    name: 'Topic'
+  },
+  {
+    id: 453,
+    name: 'MTV'
+  },
+  {
+    id: 446,
+    name: 'Retrocrush'
+  },
+  {
+    id: 194,
+    name: 'Starz Play Amazon Channel'
+  },
+  {
+    id: 445,
+    name: 'Classix'
+  },
+  {
+    id: 444,
+    name: 'Dekkoo'
+  },
+  {
+    id: 439,
+    name: 'Shout! Factory TV'
+  },
+  {
+    id: 438,
+    name: 'Chai Flicks'
+  },
+  {
+    id: 433,
+    name: 'OVID'
+  },
+  {
+    id: 427,
+    name: 'Mhz Choice'
+  },
+  {
+    id: 470,
+    name: 'The Film Detective'
+  },
+  {
+    id: 459,
+    name: 'Netflix Free'
+  },
+  {
+    id: 204,
+    name: 'Shudder Amazon Channel'
+  },
+  {
+    id: 201,
+    name: 'Mubi Amazon Channel'
+  },
+  {
+    id: 196,
+    name: 'AcornTV Amazon Channel'
+  },
+  {
+    id: 197,
+    name: 'BritBox Amazon Channel'
+  },
+  {
+    id: 199,
+    name: 'Fandor Amazon Channel'
+  },
+  {
+    id: 202,
+    name: 'Screambox Amazon Channel'
+  },
+  {
+    id: 205,
+    name: 'Sundance Now Amazon Channel'
+  },
+  {
+    id: 317,
+    name: 'Cartoon Network'
+  },
+  {
+    id: 318,
+    name: 'Adult Swim'
+  },
+  {
+    id: 322,
+    name: 'USA Network'
+  },
+  {
+    id: 328,
+    name: 'Fox'
+  },
+  {
+    id: 331,
+    name: 'FlixFling'
+  },
+  {
+    id: 343,
+    name: 'Bet+ Amazon Channel'
+  },
+  {
+    id: 528,
+    name: 'AMC Plus'
+  },
+  {
+    id: 344,
+    name: 'Rakuten Viki'
+  },
+  {
+    id: 352,
+    name: 'AMC on Demand'
+  },
+  {
+    id: 355,
+    name: 'Darkmatter TV'
+  },
+  {
+    id: 361,
+    name: 'TCM'
+  },
+  {
+    id: 365,
+    name: 'Bravo TV'
+  },
+  {
+    id: 363,
+    name: 'TNT'
+  },
+  {
+    id: 366,
+    name: 'Food Network'
+  },
+  {
+    id: 397,
+    name: 'BBC America'
+  },
+  {
+    id: 368,
+    name: 'IndieFlix'
+  },
+  {
+    id: 398,
+    name: 'AHCTV'
+  },
+  {
+    id: 412,
+    name: 'TLC'
+  },
+  {
+    id: 406,
+    name: 'HGTV'
+  },
+  {
+    id: 405,
+    name: 'DIY Network'
+  },
+  {
+    id: 408,
+    name: 'Investigation Discovery'
+  },
+  {
+    id: 411,
+    name: 'Science Channel'
+  },
+  {
+    id: 402,
+    name: 'Destination America'
+  },
+  {
+    id: 399,
+    name: 'Animal Planet'
+  },
+  {
+    id: 404,
+    name: 'Discovery Life'
+  },
+  {
+    id: 403,
+    name: 'Discovery'
+  },
+  {
+    id: 410,
+    name: 'Motor Trend'
+  },
+  {
+    id: 400,
+    name: 'Cooking Channel'
+  },
+  {
+    id: 413,
+    name: 'Travel Channel'
+  },
+  {
+    id: 418,
+    name: 'Paramount Network'
+  },
+  {
+    id: 417,
+    name: 'Here TV'
+  },
+  {
+    id: 419,
+    name: 'TV Land'
+  },
+  {
+    id: 420,
+    name: 'Logo TV'
+  },
+  {
+    id: 422,
+    name: 'VH1'
+  },
+  {
+    id: 432,
+    name: 'Flix Premiere'
+  },
+  {
+    id: 263,
+    name: 'DreamWorksTV Amazon Channel'
+  },
+  {
+    id: 506,
+    name: 'TBS'
+  },
+  {
+    id: 514,
+    name: 'AsianCrush'
+  },
+  {
+    id: 471,
+    name: 'FILMRISE'
+  },
+  {
+    id: 473,
+    name: 'Revry'
+  },
+  {
+    id: 485,
+    name: 'Rooster Teeth'
+  },
+  {
+    id: 486,
+    name: 'Spectrum On Demand'
+  },
+  {
+    id: 487,
+    name: 'OXYGEN'
+  },
+  {
+    id: 503,
+    name: 'Hi-YAH'
+  },
+  {
+    id: 504,
+    name: 'VRV'
+  },
+  {
+    id: 507,
+    name: 'tru TV'
+  },
+  {
+    id: 508,
+    name: 'DisneyNOW'
+  },
+  {
+    id: 509,
+    name: 'WeTV'
+  },
+  {
+    id: 520,
+    name: 'Discovery Plus'
+  },
+  {
+    id: 529,
+    name: 'ARROW'
+  },
+  {
+    id: 538,
+    name: 'Plex'
+  },
+  {
+    id: 547,
+    name: 'Alamo on Demand'
+  },
+  {
+    id: 555,
+    name: 'The Oprah Winfrey Network'
+  },
+  {
+    id: 562,
+    name: 'MovieSaints'
+  }
+];
