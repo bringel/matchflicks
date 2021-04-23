4c036723d70bdb2761173ed8ce0f912403dd609c
Brad Ringel <brad.ringel@gmail.com>
2021-04-23 17:27:24 -0400

    Upgrade react-native and dependencies, enable hermes and flipper on ios

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import { StackNavigationProp, createStackNavigator } from '@react-navigation/sta
 import React from 'react';
 import { View } from 'react-native';
 import { SafeAreaView, StatusBar } from 'react-native';
-import { enableScreens } from 'react-native-screens';
 import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 
 import { AuthenticationContextProvider, useAuthenticationContext } from './firebase/AuthenticationContext';
@@ -16,8 +15,6 @@ import { OnboardingModal } from './screens/Onboarding/OnboardingModal';
 import { Signup } from './screens/Signup/Signup';
 import { UserTab } from './screens/UserTab/UserTab';
 import { getColor, tailwind } from './tailwind';
-
-enableScreens();
 
 GoogleSignin.configure({
   webClientId: '479366644790-a4463vom1bpe4onn6kq6qop4avofruc8.apps.googleusercontent.com'
